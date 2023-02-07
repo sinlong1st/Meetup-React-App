23dbff8ae593177d7f924f748f91990be7512353
sinlong1st <32693869+sinlong1st@users.noreply.github.com>
2023-02-06 23:20:32 -0800

    Remove Route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import Todo from "./components/Todo";
 import Layout from "./components/layout/Layout";
 import { Route, Routes, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
